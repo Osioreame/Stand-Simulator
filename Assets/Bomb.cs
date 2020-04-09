cba6997c946a7c1f726bb288abe409e8dd46ad44
Osioreame <reameikiebe@gmail.com>
2020-04-09 06:55:29 -0400

    Added Killer Queen
    Added Kira/ killer queen and added explosions for sheer heart attack

---
+++ Assets/Bomb.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+
+    public float delay = 3f;
+    public float blastRadius = 5f;
+    public float explosionForce = 700f;
+
+    float countdown;
+    bool hasExploded;
+
+    public GameObject explosionEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        countdown = delay;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countdown -= Time.deltaTime;
+
+        if(countdown <= 0f && !hasExploded)
+        {
+            Explode();
+            hasExploded = true;
+        }
+    }
+
+    void Explode()
+    {
+        //Show effect
+        Instantiate(explosionEffect, transform.position, transform.rotation);
+
+        //Find nearby objects
+        Collider[] colliders = Physics.OverlapSphere(transform.position, blastRadius);
+
+        foreach (Collider nearbyObject in colliders)
+        {
+            //Add forces
+            Rigidbody nearbyRb = nearbyObject.GetComponent<Rigidbody>();
+
+            if(nearbyRb != null)
+            {
+                nearbyRb.AddExplosionForce(explosionForce, transform.position, blastRadius);
+            }
+        }
+
+        
+
+        //Remove grenade
+        //Destroy(GameObject);
+    }
+}
