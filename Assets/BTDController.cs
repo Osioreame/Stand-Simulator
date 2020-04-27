837b30bc5b4290375b84df63c132145c600a7ebe
Osioreame <reameikiebe@gmail.com>
2020-04-26 22:35:41 -0400

    Added homing function to Sheer Heart Attack

---
+++ Assets/BTDController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BTDController : MonoBehaviour
+{
+    public GameObject player;
+    NavMeshAgent btd;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        btd = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        btd.SetDestination(player.transform.position);
+    }
+
+
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Game Over");
+    }
+
+
+    public void SlowEnemy()
+    {
+        btd.GetComponent<NavMeshAgent>().speed = 0.1f;
+    }
+
+
+}
